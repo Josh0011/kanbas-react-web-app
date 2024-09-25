27b5b3ccb8f170659b91ad61a77beee76f705c1d
Josh0011 <joshuasharma4@gmail.com>
2024-09-24 23:07:21 -0400

    Final Assignment

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
             <li><Link to="/Labs/Lab2">Lab 2</Link></li>
             <li><Link to="/Labs/Lab3">Lab 3</Link></li>
             <li><Link to="/Kanbas">Kanbas</Link></li>
+            <li><Link to="https://github.com/Josh0011/kanbas-react-web-app">Github</Link></li>
         </ul>
     );
 }
